5fdc2f77ef568f6e33e4f5b4bad661bc04b73983
ashfinenayet <ashfin.enayet@gmail.com>
2020-07-10 18:35:14 -0400

    test SQLITE

--- index.js
+++ index.js
@@ -61,44 +61,129 @@ for (const file of commandFiles) {
 client.on("ready", () => {
     client.user.setActivity("You", { type: "WATCHING" })
     console.log(`Logged in as ${client.user.tag}!`);
-    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
-    if (!table['count(*)']) {
-      // If the table isn't there, create it and setup the database correctly.
-      sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
-      // Ensure that the "id" row is always unique and indexed.
-      sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
-      sql.pragma("synchronous = 1");
-      sql.pragma("journal_mode = wal");
-    }
-  
-    // And then we have two prepared statements to get and set the score data.
-    client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
-    client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
+   
   });
 
-
+  const Enmap = require("enmap");
+  client.points = new Enmap({name: "points"});
 
 //const serverQueue = queue.get(message.guild.id);
-client.on("message", message => {
-  if (message.author.bot) return;
-  let score;
+client.on("message", async (message) => {
+  // First, ignore itself and all other bots. Also, ignore private messages so a user can't spam the bot for points.
+  if (!message.guild || message.author.bot) return;
+
+  /* Now we start the real code for this tutorial */
+
+  // If this is not in a DM, execute the points code.
   if (message.guild) {
-    score = client.getScore.get(message.author.id, message.guild.id);
-    if (!score) {
-      score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1 }
-    }
-    score.points++;
-    const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
-    if(score.level < curLevel) {
-      score.level++;
+    // We'll use the key often enough that simplifying it is worth the trouble.
+    const key = `${message.guild.id}-${message.author.id}`;
+    
+    // Triggers on new users we haven't seen before.
+    client.points.ensure(key, {
+      user: message.author.id,
+      guild: message.guild.id,
+      points: 0,
+      level: 1,
+      lastSeen: new Date()
+    });
+    
+    // Increment the points and save them.
+    client.points.inc(key, "points");
+    
+    // Calculate the user's current level
+    const curLevel = Math.floor(0.1 * Math.sqrt(client.points.get(key, "points")));
+
+    // Act upon level up by sending a message and updating the user's level in enmap.
+    if (client.points.get(key, "level") < curLevel) {
       message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
+      client.points.set(key, curLevel, "level");
     }
-    client.setScore.run(score);
   }
   
+  /* END POINTS ATTRIBUTION. Now let's have some fun with commands. */
 
+  // As usual, we stop processing if the message does not start with our prefix.
   
-  // Command-specific code here!
+
+  // Also we use the config prefix to get our arguments and command:
+  
+  
+  // Let's build some useful ones for our points system.
+
+  if (message.content.startsWith(`${prefix}point`)) {
+    const key = `${message.guild.id}-${message.author.id}`;
+    return message.channel.send(`You currently have ${client.points.get(key, "points")} points, and are level ${client.points.get(key, "level")}!`);
+  }
+
+  if(message.content.startsWith(`${prefix}leaderboard`)) {
+    // Get a filtered list (for this guild only), and convert to an array while we're at it.
+    const filtered = client.points.array().filter( p => p.guild === message.guild.id );
+  
+    // Sort it to get the top results... well... at the top. Y'know.
+    const sorted = filtered.sort((a, b) => a.points < b.points);
+  
+    // Slice it, dice it, get the top 10 of it!
+    const top10 = sorted.splice(0, 10);
+  
+    // Now shake it and show it! (as a nice embed, too!)
+    const embed = new Discord.RichEmbed()
+      .setTitle("Leaderboard")
+      .setAuthor(client.user.username, client.user.avatarURL)
+      .setDescription("Our top 10 points leaders!")
+      .setColor(0x00AE86);
+    for(const data of top10) {
+      embed.addField(client.users.get(data.user).tag, `${data.points} points (level ${data.level})`);
+    }
+    return message.channel.send({embed});
+  }
+
+  if(message.content.startsWith(`${prefix}give`)) {
+    // Limited to guild owner - adjust to your own preference!
+    if(!message.author.id === message.guild.owner) return message.reply("You're not the boss of me, you can't do that!");
+
+    const user = message.mentions.users.first() || client.users.get(args[0]);
+    if(!user) return message.reply("You must mention someone or give their ID!");
+
+    const pointsToAdd = parseInt(args[1], 10);
+    if(!pointsToAdd) return message.reply("You didn't tell me how many points to give...");
+    
+    const key = `${message.guild.id}-${user.id}`;
+
+    // Ensure there is a points entry for this user.
+    client.points.ensure(key, {
+      user: message.author.id,
+      guild: message.guild.id,
+      points: 0,
+      level: 1,
+      lastSeen: new Date()
+    });
+    
+    // Add the points to the enmap for this user.
+    client.points.math(key, "+", pointsToAdd, "points");
+
+    message.channel.send(`${user.tag} has received ${pointsToAdd} points and now stands at ${client.points.get(key, "points")} points.`);
+  }
+
+  if(message.content.startsWith(`${prefix}cleanup`)) {
+    // Let's clean up the database of all "old" users, and those who haven't been around for... say a month.
+
+    // Get a filtered list (for this guild only).
+    const filtered = client.points.filter( p => p.guild === message.guild.id );
+
+    // We then filter it again (ok we could just do this one, but for clarity's sake...)
+    // So we get only users that haven't been online for a month, or are no longer in the guild.
+    const rightNow = new Date();
+    const toRemove = filtered.filter(data => {
+      return !message.guild.members.has(data.user) || rightNow - 2592000000 > data.lastSeen;
+    });
+
+    toRemove.forEach(data => {
+      client.points.delete(`${message.guild.id}-${data.user}`);
+    });
+
+    message.channel.send(`I've cleaned up ${toRemove.size} old farts.`);
+  }
 });
 
 
