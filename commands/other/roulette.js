8af7f0f13c64390c53aa208b24364b2bb9d008f9
ashfinenayet <ashfin.enayet@gmail.com>
2020-07-23 20:04:08 -0400

    added dice and roulette commands

---
+++ commands/other/roulette.js
@@ -0,0 +1,29 @@
+
+const { Command } = require('discord.js-commando');
+
+module.exports = class RouletteCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'roulette',
+      group: 'other',
+      memberName: 'roulette',
+      description: 'live life on the edge(for safety reasons its a watergun)',
+      throttling: {
+        usages: 1,
+        duration: 6
+      }
+    });
+  }
+
+  run(message) {
+	let random = Math.random() * 100;
+	// console.log(random);
+	if (random < 100 / 6) {
+	  message.channel.send('🔥🔫 You died.');
+	  userToMute.addRole(muteRole);
+	  setTimeout(() => userToMute.removeRole(muteRole), muteTime);
+	} else {
+	  message.channel.send('🚬🔫 You\'re safe... For now...');
+	}
+  }
+};
